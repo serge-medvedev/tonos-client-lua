7e7de3cd62a6b38b22f48795e38aff21e005c63d
Serge Medvedev <thismoment.main@gmail.com>
2020-10-01 02:12:52 +0300

    feat(boc): methods are implemented; basic tests are added

--- boc/boc.lua
+++ boc/boc.lua
@@ -3,9 +3,9 @@ local json = require "json"
 
 local boc = {}
 
-function boc.parse_message(ctx, boc)
+function parse(ctx, method, boc)
     local params_json = json.encode({ boc = boc })
-    local response_handle = tc.json_request(ctx, "boc.parse_message", params_json)
+    local response_handle = tc.json_request(ctx, method, params_json)
     local err, result = tc.read_json_response(response_handle)
 
     if err then
@@ -13,6 +13,22 @@ function boc.parse_message(ctx, boc)
     end
 
     return json.decode(result)
+end
+
+function boc.parse_transaction(ctx, boc) 
+    return parse(ctx, "boc.parse_transaction", boc)
+end
+
+function boc.parse_block(ctx, boc)
+    return parse(ctx, "boc.parse_block", boc)
+end
+
+function boc.parse_account(ctx, boc)
+    return parse(ctx, "boc.parse_account", boc)
+end
+
+function boc.parse_message(ctx, boc)
+    return parse(ctx, "boc.parse_message", boc)
 end
 
 return boc
