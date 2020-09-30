a01fe1c2848ac62136032b6f7516f1c562c934cc
Serge Medvedev <thismoment.main@gmail.com>
2020-10-01 01:31:06 +0300

    feat(abi): methods are implemented; basic tests ain't working

--- abi/abi.lua
+++ abi/abi.lua
@@ -3,10 +3,9 @@ local json = require "json"
 
 local abi = {}
 
-function abi.encode_message(ctx, abi, address, deploy_set, call_set, signing)
-    local params_json = json.encode(
-        { abi = abi, address = address, deploy_set = deploy_set, call_set = call_set, signing = signing })
-    local response_handle = tc.json_request(ctx, "abi.encode_message", params_json)
+function abi.decode_message(ctx, abi, message)
+    local params_json = json.encode({ abi = abi, message = message })
+    local response_handle = tc.json_request(ctx, "abi.decode_message", params_json)
     local err, result = tc.read_json_response(response_handle)
 
     if err then
@@ -16,9 +15,23 @@ function abi.encode_message(ctx, abi, address, deploy_set, call_set, signing)
     return json.decode(result)
 end
 
-function abi.decode_message(ctx, abi, message)
-    local params_json = json.encode({ abi = abi, message = message })
-    local response_handle = tc.json_request(ctx, "abi.decode_message", params_json)
+function abi.attach_signature(ctx, abi, public_key, message, signature)
+    local params_json = json.encode(
+		{ abi = abi, public_key = public_key, message = message, signature = signature })
+    local response_handle = tc.json_request(ctx, "abi.attach_signature", params_json)
+    local err, result = tc.read_json_response(response_handle)
+
+    if err then
+        error(err)
+    end
+
+    return json.decode(result)
+end
+
+function abi.encode_message(ctx, abi, address, deploy_set, call_set, signing)
+    local params_json = json.encode(
+        { abi = abi, address = address, deploy_set = deploy_set, call_set = call_set, signing = signing })
+    local response_handle = tc.json_request(ctx, "abi.encode_message", params_json)
     local err, result = tc.read_json_response(response_handle)
 
     if err then
