cb64f09fe69a2e0838754411944430fb486be505
Serge Medvedev <thismoment.main@gmail.com>
2020-10-03 01:18:42 +0300

    wip(tvm): spec skeleton is introduced

---
+++ tvm/_spec.lua
@@ -0,0 +1,22 @@
+pending("a tvm test suite #tvm", function()
+    local context = require "context"
+    local tvm = require "tvm"
+
+    local ctx
+
+    setup(function()
+        local config = '{"network": {"server_address": "https://net.ton.dev"}}'
+
+        ctx = context.create(config).handle
+    end)
+
+    teardown(function()
+        context.destroy(ctx)
+    end)
+
+    describe("a tvm.estimate_fees", function()
+        it("should return estimated fees", function()
+        end)
+    end)
+end)
+
