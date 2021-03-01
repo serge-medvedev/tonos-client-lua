a255ce208b47ff40b72f957060f5b19bfeb19881
Serge Medvedev <thismoment.main@gmail.com>
2021-03-01 18:03:05 +0200

    chore: version bumped up

--- spec/client_spec.lua
+++ spec/client_spec.lua
@@ -20,7 +20,7 @@ describe("a client test suite #client", function()
         it("should return SDK version", function()
             local result = client.version(ctx).await()
 
-            assert.equals("1.8.0", result.version)
+            assert.equals("1.9.0", result.version)
         end)
     end)
 
